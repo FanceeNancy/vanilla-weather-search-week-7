1e4baf389c25dfd107edd70f65e781366d1b756e
FanceeNancy <nancy.melchert@gmail.com>
2024-11-22 17:25:27 -0500

    added condition if city entered does not exist

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ function searchCity(city) {
   // make api call and update the interface
   let apiKey = "0de0bof3a7bt5b0870ae4284e12b3ada";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-  //console.log(apiURL);
   axios.get(apiURL).then(refreashWeatherData);
 }
 
@@ -42,39 +41,46 @@ function formatDate(date) {
 }
 
 function refreashWeatherData(response) {
+  //   console.log(response);
   console.log(response.data);
 
-  let country = response.data.country;
-  console.log(typeof country);
+  if (response.data.message === "City not found") {
+    console.log(`${response.data.message}. Please enter a new city. `);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = `${response.data.message}. Please enter a new city. `;
+  } else {
+    let country = response.data.country;
+    //console.log(typeof country);
 
-  let temperatureElement = document.querySelector("#temperature");
-  let temperature = Math.round(response.data.temperature.current);
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = Math.round(response.data.temperature.current);
 
-  let cityElement = document.querySelector("#city");
-  let city = response.data.city;
+    let cityElement = document.querySelector("#city");
+    let city = response.data.city;
 
-  let humidityElement = document.querySelector("#humid");
-  let humidity = `${response.data.temperature.humidity}%`;
+    let humidityElement = document.querySelector("#humid");
+    let humidity = `${response.data.temperature.humidity}%`;
 
-  let windElement = document.querySelector("#wind");
-  let wind = `${response.data.wind.speed}mph`;
+    let windElement = document.querySelector("#wind");
+    let wind = `${response.data.wind.speed}mph`;
 
-  let weatherIconElement = document.querySelector("#weather-icon");
-  let weatherIcon = response.data.condition.icon_url;
+    let weatherIconElement = document.querySelector("#weather-icon");
+    let weatherIcon = response.data.condition.icon_url;
 
-  let descriptionElement = document.querySelector("#description");
-  let description = response.data.condition.description;
+    let descriptionElement = document.querySelector("#description");
+    let description = response.data.condition.description;
 
-  let currentDateELement = document.querySelector("#current-date");
-  let currentDate = new Date();
+    let currentDateELement = document.querySelector("#current-date");
+    let currentDate = new Date();
 
-  cityElement.innerHTML = city;
-  temperatureElement.innerHTML = temperature;
-  humidityElement.innerHTML = humidity;
-  windElement.innerHTML = wind;
-  weatherIconElement.innerHTML = `<img src = "${weatherIcon}" alt="">`;
-  descriptionElement.innerHTML = description;
-  currentDateELement.innerHTML = formatDate(currentDate);
+    cityElement.innerHTML = city;
+    temperatureElement.innerHTML = temperature;
+    humidityElement.innerHTML = humidity;
+    windElement.innerHTML = wind;
+    weatherIconElement.innerHTML = `<img src = "${weatherIcon}" alt="">`;
+    descriptionElement.innerHTML = description;
+    currentDateELement.innerHTML = formatDate(currentDate);
+  }
 }
 
 let searchFormElement = document.querySelector("#search-form");
